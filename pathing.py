eae0bcd81a257c026ef5b42892c81a58c71b0bf4
23clhelle <chellebusch2@unl.edu>
2024-10-02 18:37:52 -0500

    to target works!!

--- pathing.py
+++ pathing.py
@@ -18,8 +18,8 @@ def get_test_path():
 def get_random_path():
     graph_index = int(global_game_data.current_graph_index)
     graph = graph_data.graph_data[graph_index]
-    #random_path_to_target = generate_random_path(graph, 0, global_game_data.target_node[graph_index])
-    #random_path_to_exit = generate_random_path(graph, global_game_data.target_node[graph_index], graph[len(graph) - 1][1])
+    random_path_to_target = generate_random_path(graph, 0, global_game_data.target_node[graph_index])
+    #random_path_to_exit = generate_random_path(graph, global_game_data.target_node[graph_index], len(graph) - 1)
 
     #if random_path_to_target is None :
      #   if random_path_to_exit is None : 
@@ -31,8 +31,8 @@ def get_random_path():
      #   return random_path_to_target
 
     #random_path = random_path_to_target[:-1] + random_path_to_exit   
-    #return random_path_to_target
-    return [1,2]
+    return random_path_to_target
+    #return [1,2]
 
 
 def get_dfs_path():
@@ -46,20 +46,24 @@ def get_bfs_path():
 def get_dijkstra_path():
     return [1,2]
 
-def generate_random_path(graph, start, end):
+def generate_random_path(graph, start, target):
     path = []
     path.append(start)
-    
     curr_node = start
 
-    while curr_node != end :
-        curr_node = path[len(path) - 1]
-        neighbors = graph[curr_node][1]
-        #valid_neighbors = neighbors[:]
-        #for node in path:
-        #    valid_neighbors.remove(node)
-        valid_neighbors = list(set(neighbors) - set(path))
-        next_node = random.choice(valid_neighbors)
+    while curr_node != target :
+        neighbors = list(graph[curr_node][1])
+        
+        valid_neighbors = neighbors[:]
+        for node in path:
+            if node in valid_neighbors:
+                valid_neighbors.remove(node)
+        
+        if not valid_neighbors:  # No valid neighbors, the path is stuck
+            print("ERROR")
+            return None
+        
+        next_node = int(random.choice(valid_neighbors))
         path.append(next_node)
-
+        curr_node = path[-1]
     return path
