dfd39c4d580d8c4647ae355ce6107469732ddbff
23clhelle <chellebusch2@unl.edu>
2024-10-22 21:33:05 -0500

    oml

--- pathing.py
+++ pathing.py
@@ -21,7 +21,7 @@ def get_random_path():
     graph_index = int(global_game_data.current_graph_index)
     graph = graph_data.graph_data[graph_index]
 
-    '''
+    
     random_path_to_target = generate_random_path(graph, 0, global_game_data.target_node[graph_index])
     random_path_to_exit = generate_random_path(graph, global_game_data.target_node[graph_index], len(graph) - 1)
 
@@ -29,35 +29,41 @@ def get_random_path():
     assert(path_is_valid(graph, random_path))   
     
     return random_path
-    '''
-
-    return [1,2]
+    
 
 
 def get_dfs_path():
-    '''
     graph_index = int(global_game_data.current_graph_index)
     graph = graph_data.graph_data[graph_index]
     target = global_game_data.target_node[graph_index]
-    print(graph)
+
+    visited = [False] * len(graph)
     
-    dfs_path_to_target = generate_dfs_path(graph, 0, target)
-    dfs_path_to_end = generate_dfs_path(graph, target, len(graph) - 1)
+    dfs_path_to_target = generate_dfs_path(graph, 0, target, visited)
+
+    for i in dfs_path_to_target:
+        visited[i] = True
+    visited[target] = False
+
+
+    dfs_path_to_end = generate_dfs_path(graph, target, len(graph) - 1, visited)
 
     dfs_path = dfs_path_to_target + dfs_path_to_end[1:]
 
     assert(target in dfs_path)
     assert(dfs_path[-1] == len(graph) - 1)
 
+    return dfs_path
+
+'''
     for i in range(len(dfs_path) - 2):
         next = i + 1
-        print(graph[i][1])
         assert(next in graph[i][1])
 
     return dfs_path
     '''
-    return [1,2]
-    
+    #return [1,2]
+
 
 
 def get_bfs_path():
@@ -126,6 +132,7 @@ def generate_random_path(graph, start, target):
 
 '''
 def generate_dfs_path(graph, start, target):
+    print("CALLED GENERATE")
     visited = [False] * (len(graph))
     s = []
     path = []
@@ -146,33 +153,112 @@ def generate_dfs_path(graph, start, target):
                 s.append(u)
                 visited[neighbor] = True
                 s.append(neighbor)
-                
-                
-    print(path)
-'''
 
-def generate_dfs_path(graph, start, target):
-    visited = [False] * len(graph)
-    s = [((start, [start]))]
+'''
+def generate_dfs_path(graph, start, target, visited):
+    s = [start]  # Stack for DFS
+    path = []    # Store the path taken
 
     while s:
-        # Get the current node and the path to it
-        u, current_path = s.pop()
-        
-        # if reached the target, return the path
+        u = s.pop()
+
+        # If we have reached the target, return the path
         if u == target:
-            return current_path
-        
+            path.append(u)  # Add the target to the path
+            return path  # Return the full path when the target is found
+
         if not visited[u]:
-            visited[u] = True
-            
-            # check neighbors
+            visited[u] = True  # Mark the node as visited
+            path.append(u)  # Add to the current path
+
+            # Check all neighbors of the current node
             neighbors = graph[u][1]
             for neighbor in neighbors:
                 if not visited[neighbor]:
-                    s.append((neighbor, current_path + [neighbor]))
+                    s.append(neighbor)  # Only add unvisited neighbors to the stack
+                    break  # Explore only one neighbor at a time
+        else:
+            # Backtrack by removing the last node added to the path
+            path.pop()
 
-    return [1]
+    print("No path found from", start, "to", target)
+    return []  # Return an empty list if no path is found
+
+
+
+
+
+'''    
+def generate_dfs_path(graph, start, target, visited):
+    path = []
+    
+    # Recursive DFS helper
+    def dfs(node):
+        # Add the current node to the path
+        path.append(node)
+        print(f"Visiting node {node}, current path: {path}")
+        
+        # If we reached the target, return True to indicate success
+        if node == target:
+            print(f"Target {target} found!")
+            return True
+        
+        # Mark the current node as visited
+        visited[node] = True
+        
+        # Explore all neighbors of the current node
+        for neighbor in graph[node][1]:
+            if not visited[neighbor]:
+                print(f"Exploring neighbor {neighbor} of node {node}")
+                # Recursively call DFS on the neighbor
+                if dfs(neighbor):
+                    return True  # If target is found, unwind and stop searching
+        
+        # If no valid path was found from this node, backtrack
+        print(f"Backtracking from node {node}, current path: {path}")
+        if node == target:
+            return True
+        path.pop()  # Remove the node from the path as we backtrack
+        return False
+    
+    # Start DFS from the start node
+    if dfs(start):
+        return path
+    else:
+        # If no path is found, return an empty list
+        print(f"No valid path found from {start} to {target}")
+        return []
+'''
+'''
+def generate_dfs_path(graph, start, target, visited):
+    
+    s = [(start)]
+    path = [(start)]
+
+    while s:
+        u = s.pop()
+        
+        # if reached the target, return the path
+        if u == target:
+            path.append(u)
+            print(f"Target {target} found!")
+            return path
+        
+        if not visited[u]:
+            visited[u] = True
+
+            neighbors = graph[u][1]
+            for neighbor in neighbors:
+                if not visited[neighbor]:
+                    s.append(neighbor)
+                    path.append(neighbor)
+                    break
+        else:
+            # Backtracking
+            path.pop()
+
+    return path
+'''
 
 
 def generate_bfs_path(graph, start, target):
