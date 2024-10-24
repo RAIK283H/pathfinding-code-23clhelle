eedf7513a253b2874d88cb85a166181986d48a0a
23clhelle <chellebusch2@unl.edu>
2024-10-23 21:44:26 -0500

    WORD

--- main2.py
+++ main2.py
@@ -3,8 +3,16 @@ import permutation
 
 graph = graph_data.graph_data[0]
 
-permutations = permutation.determine_sjt(len(graph))
+for graph_index in range(len(graph_data.graph_data) - 1):
+    graph = graph_data.graph_data[graph_index]
+    print("Graph Index: ", graph_index)
 
-for perm in permutations:
-    str_perm = ''.join(str(num) for num in perm)
-    print(str_perm)
+    permutations = permutation.determine_sjt(len(graph))
+
+    count = 0
+    for perm in permutations:
+        str_perm = ''.join(str(num) for num in perm)
+        print(str_perm)
+        count = count + 1
+    
+    print("")
