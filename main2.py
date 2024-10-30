4d845022a5c8c60d13a33b4606c226501e0ec2da
23clhelle <chellebusch2@unl.edu>
2024-10-30 17:34:28 -0500

    EXTRA CREDIT

--- main2.py
+++ main2.py
@@ -1,5 +1,6 @@
 import graph_data
 import permutation
+import math
 
 graph = graph_data.graph_data[0]
 
@@ -37,16 +38,43 @@ def check_for_hamiltonian(graph):
         return cycles
     else:
         return -1
+    
+def shortest_cycle(cycles, graph):
+    distance = float('inf')
+    shortest_cycle = []
+    for cycle in cycles:
+        cycle_distance = 0
+        for i in range(len(cycle) - 2):
+            node_x = graph[i][0][0]
+            node_y = graph[i][0][1]
+
+            next_node_x = graph[i+1][0][0]
+            next_node_y = graph[i+1][0][1]
+
+            distance = math.sqrt(pow(node_x - next_node_x, 2) + pow(node_y - next_node_y, 2))
+            cycle_distance = cycle_distance + distance
+        if cycle_distance < distance:
+            distance = cycle_distance
+        shortest_cycle = cycles[i]
+        
+    
+    return shortest_cycle
+
 
 for graph_index in range(len(graph_data.graph_data) - 1):
     graph = graph_data.graph_data[graph_index]
     print("Graph Index: ", graph_index)
     cycles = check_for_hamiltonian(graph)
+    
         
     if cycles == -1:
         print(-1)
     else:
         for perm in cycles:
             print(perm)
+        
+        #Extra Credit
+        shortest = shortest_cycle(cycles, graph)
+        print("Shortest Cycle: ", shortest)
     
     print("")
