14cc9072a6d542d033c1bbe44ffd681c7f86dd12
23clhelle <chellebusch2@unl.edu>
2024-11-25 12:23:36 -0600

    floyd and matrix function

--- f_w.py
+++ f_w.py
@@ -1,7 +1,21 @@
-def floyd_warshall(W):
+def adjacency_list_to_matrix(adj_list, n):
+    matrix = [[float('inf')] * n for _ in range(n)]
     
+    for i in range(n):
+        matrix[i][i] = 0
+    
+    for u, neighbors in adj_list.items():
+        for v, weight in neighbors:
+            matrix[u][v] = weight
+    
+    return matrix
+
+def floyd_warshall(W):
+
+    distance = [row[:] for row in W]
+
     for k in range(len()):
         for i in range(len()):
             for j in range(len()):
-                return
-    return 1
+                distance[i][j] = min(distance[i][j], distance[i][k] + distance[k][j])
+    return distance
